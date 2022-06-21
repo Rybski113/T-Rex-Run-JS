9b6d4a111b0e2bc9b147e5a16fbf1d67d6aa3eac
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-06-21 08:17:53 +0200

    Making HTML/CSS and some JS

---
+++ app.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const dino = document.querySelector('.dino')
+
+    function control(e) {
+        if(e.keyCode === 32) {
+            jump()
+        }
+    }
+    document.addEventListener('keyup', control)
+
+    function jump() {
+        let position = 0
+       let timerId = setInterval(function () {
+
+
+        //move down
+        if (position === 150) {
+            clearInterval(timerId)
+            console.log('down')
+
+            let downTimerId = setInterval(function() {
+                if(position === 0) {
+                    clearInterval(downTimerId)
+                }
+                position -=30
+                dino.style.bottom = position + 'px'
+            },20)
+
+            
+        }
+
+
+        // move up
+        console.log('up')
+        position +=30
+        dino.style.bottom = position + 'px'
+       },20)
+    }
+
+
+})
